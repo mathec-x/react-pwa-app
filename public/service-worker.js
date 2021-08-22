90d8b3547e81b712dd4795fb55634772762f3f3a
matheus correa <mathec@live.com>
2021-08-22 18:36:39 -0300

    fix: ajust service worker script

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,82 +1,79 @@
- var CACHE_NAME = '@cacheName';
- var urlsToCache = ['/','/styles/styles.css','/script/webpack-bundle.js'];
- var appIcon = 'default_icon_link';
- 
- //array routers on fetch not will cache
- const uri_black_list = [];
- //https://developers.google.com/web/ilt/pwa/introduction-to-push-notifications#request_permission
- 
- const urlB64ToUint8Array = base64String => {
-   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
-   const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
-   const rawData = atob(base64)
-   const outputArray = new Uint8Array(rawData.length)
-   for (let i = 0; i < rawData.length; ++i) {
-     outputArray[i] = rawData.charCodeAt(i)
-   }
-   return outputArray
- }
- 
- const SubscriptionOptions = {
-   applicationServerKey: urlB64ToUint8Array(
-     'get_public_key_on_firebase'
-   ), userVisibleOnly: true
- }
- 
- self.addEventListener('install', function (event) {
-   event.waitUntil(
-     caches.open(CACHE_NAME)
-       .then(function (cache) {
-         console.log('[serviceWorker] add all cache from array');
-         return cache.addAll(urlsToCache);
-       })
-   );
- });
- 
- self.addEventListener('activate', function (event) {
-   //ativa steps
-   console.log('[serviceWorker] ativo');
-   event.waitUntil(
-     //registrar o push
+const CACHE_NAME = '@cacheName';
+const urlsToCache = ['/', '/styles/styles.css', '/script/webpack-bundle.js'];
+const appIcon = 'default_icon_link';
+
+// array routers on fetch not will cache
+const uri_black_list = [];
+// https://developers.google.com/web/ilt/pwa/introduction-to-push-notifications#request_permission
+
+const urlB64ToUint8Array = (base64String) => {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
+  const rawData = atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+};
+
+const SubscriptionOptions = {
+  applicationServerKey: urlB64ToUint8Array(
+    'get_public_key_on_firebase',
+  ),
+  userVisibleOnly: true,
+};
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('[serviceWorker] add all cache from array');
+        return cache.addAll(urlsToCache);
+      }),
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  // ativa steps
+  console.log('[serviceWorker] ativo');
+  event.waitUntil(
+    // registrar o push
     //  self.registration.pushManager.subscribe(SubscriptionOptions),
-     //limpar versão antiga
-     caches.keys().then(function (cacheNames) {
-       return Promise.all(cacheNames.map(function (thisCacheName) {
-         if (thisCacheName !== CACHE_NAME) {
-           console.log("[Service Worker] Removing caching files from", thisCacheName);
-           return caches.delete(thisCacheName);
-         }
- 
-         return false;
-       }))
-     })
- 
-   );
- 
- });
- 
- /**
-  * cache all fetch data make inside app, this is not really usefull in some cases
-  */
- self.addEventListener('fetch', (e) => {
-   e.respondWith((async () => {
-     const r = await caches.match(e.request);
-     if (r) { return r; }
- 
-     const response = await fetch(e.request);
- 
-     // posts, put, delete are not allowed to cache
-     if(e.request.method === 'GET' && !uri_black_list.includes(e.request.url)){
-       console.log(`[Service Worker ${e.request.method}] Caching new resource from ${CACHE_NAME}: ${e.request.url}`);      
-       const cache = await caches.open(CACHE_NAME);
-       cache.put(e.request, response.clone());
-     }
- 
-     return response;
-   })());
- 
- });
- 
+    // limpar versão antiga
+    caches.keys().then((cacheNames) => Promise.all(cacheNames.map((thisCacheName) => {
+      if (thisCacheName !== CACHE_NAME) {
+        console.log('[Service Worker] Removing caching files from', thisCacheName);
+        return caches.delete(thisCacheName);
+      }
+
+      return false;
+    }))),
+
+  );
+});
+
+/**
+ * cache all fetch data make inside app, this is not really usefull in some cases
+ */
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    if (r) { return r; }
+
+    const response = await fetch(e.request);
+
+    // posts, put, delete are not allowed to cache
+    if (e.request.method === 'GET' && !uri_black_list.includes(e.request.url)) {
+      console.log(`[Service Worker ${e.request.method}] Caching new resource from ${CACHE_NAME}: ${e.request.url}`);
+      const cache = await caches.open(CACHE_NAME);
+      cache.put(e.request, response.clone());
+    }
+
+    return response;
+  })());
+});
+
 //  self.addEventListener("push", function (event) {
 //    /**@type {{body: string, title: string, icon: string?}} message */
 //    const message = event.data.json();
@@ -98,13 +95,12 @@
 //          },
 //        ]
 //      };
- 
+
 //      event.waitUntil(
 //        self.registration.showNotification(message.title, options)
 //      );
 //  });
- 
- 
+
 //  self.addEventListener("notificationclick", function openPushNotification(event) {
 //      console.log("Notification click Received.", event.notification.data);
 //     event.notification.close();
