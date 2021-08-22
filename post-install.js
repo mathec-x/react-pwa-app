a944c617accc760f9552d4f77bfdb85f5011bbee
matheus correa <mathec@live.com>
2021-08-22 18:32:09 -0300

    feat: try add a post install

--- post-install.js
+++ post-install.js
@@ -1,18 +1,24 @@
+// post-install.js
+
+/**
+ * Script to run after npm install
+ *
+ * Copy selected files to user's directory
+ */
+
+'use strict'
+
 const fs = require('fs');
 const path = require('path');
 
-const PostInstall = async () => {
-    const publicdir = path.resolve(process.env.INIT_CWD, "public");
-    const currentdir = path.resolve(__dirname, "node_modules", "react-pwa-app", "public");
 
-    if (!fs.existsSync(publicdir + "/manifest.webmanifest")){
-        fs.copyFileSync(currentdir + "/manifest.webmanifest", publicdir + "/manifest.webmanifest");
-    }
+const publicdir = path.resolve(process.env.INIT_CWD, "public");
+const currentdir = path.resolve(__dirname, "node_modules", "react-pwa-app", "public");
 
-    if (!fs.existsSync(publicdir + "/service-worker.js")){
-        fs.copyFileSync(currentdir + "/service-worker.js", publicdir + "/service-worker.js");
-    }
-
+if (!fs.existsSync(publicdir + "/manifest.webmanifest")) {
+    fs.copyFileSync(currentdir + "/manifest.webmanifest", publicdir + "/manifest.webmanifest");
 }
 
-PostInstall();
+if (!fs.existsSync(publicdir + "/service-worker.js")) {
+    fs.copyFileSync(currentdir + "/service-worker.js", publicdir + "/service-worker.js");
+}
